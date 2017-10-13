23f6e69c51e2cd90ef8c5782517dbd5989766d04
Kat Marchán <kzm@sykosomatic.org>
2017-10-13 00:51:45 -0400

    feat(api): additional utility functions for dealing with trees

--- test/index.js
+++ test/index.js
@@ -1,5 +1,8 @@
 'use strict'
 
+const BB = require('bluebird')
+
+const path = require('path')
 const test = require('tap').test
 
 const logi = require('../index.js')
@@ -166,7 +169,242 @@ test('supports dependency cycles', t => {
   t.done()
 })
 
-test('UNIT: atAddr', t => {
+test('addDep', t => {
+  const tree = logi.node('foo')
+  const dep = logi.node('bar')
+  t.equal(tree.addDep(dep), tree, 'returns the tree')
+  t.deepEqual(
+    tree.dependencies,
+    new Map([[dep.name, dep]]),
+    'dep added to dependencies'
+  )
+  t.deepEqual(dep.requiredBy, new Set([tree]), 'requiredBy updated for dep')
+  t.deepEqual(dep.dependencies, new Map(), 'nothing in dep.dependencies')
+  t.deepEqual(tree.requiredBy, new Set(), 'nothing in tree.requiredBy')
+  t.done()
+})
+
+test('delDep', t => {
+  const tree = logi.node('foo')
+  const dep = logi.node('bar')
+  tree.addDep(dep)
+  t.equal(tree.delDep(dep), tree, 'returns the tree')
+  t.deepEqual(tree.dependencies, new Map(), 'nothing in tree.dependencies')
+  t.deepEqual(dep.requiredBy, new Set(), 'nothing in dep.requiredBy')
+  t.done()
+})
+
+test('getDep', t => {
+  const tree = logi.node('foo')
+  const dep = logi.node('bar')
+  tree.addDep(dep)
+  t.equal(tree.getDep('bar'), dep, 'got dep named bar')
+  t.equal(tree.getDep('baz'), undefined, 'nothing if no dep w/ that name')
+  t.done()
+})
+
+test('path', t => {
+  const tree = logi.node('foo')
+  .addDep(logi.node('bar', 'bar'))
+  .addDep(logi.node('baz', 'baz:quux:hey'))
+  .addDep(logi.node('quux', 'quux:@meh/x:oh'))
+  t.equal(tree.path(), '', 'root node has no address by default')
+  t.equal(
+    tree.path(path.join('foo', 'bar')),
+    path.join('foo', 'bar'),
+    'root node path uses prefix directly if passed in'
+  )
+  t.equal(
+    tree.getDep('bar').path(),
+    path.join('node_modules', 'bar'),
+    'first-level dep uses own name with n_m prepended as path'
+  )
+  t.equal(
+    tree.getDep('bar').path(path.join('foo', 'bar')),
+    path.join('foo', 'bar', 'node_modules', 'bar'),
+    'prefix and node_modules prepended to first-level dep'
+  )
+  t.equal(
+    tree.getDep('baz').path(),
+    path.join(
+      'node_modules', 'baz', 'node_modules', 'quux', 'node_modules', 'hey'
+    ),
+    'paths include node_modules for nested deps'
+  )
+  t.equal(
+    tree.getDep('quux').path(),
+    path.join(
+      'node_modules', 'quux', 'node_modules', '@meh', 'x', 'node_modules', 'oh'
+    ),
+    'supports scoped dependencies'
+  )
+  t.done()
+})
+
+test('logicalTree.node', t => {
+  const tree = logi.node('hey')
+  t.similar(tree, {
+    name: 'hey',
+    version: undefined,
+    address: '',
+    optional: false,
+    dev: false,
+    bundled: false,
+    resolved: undefined,
+    integrity: undefined,
+    dependencies: new Map(),
+    requiredBy: new Set()
+  }, 'default construction')
+  t.equal(tree.isRoot, true, 'detected as root')
+  const dep = logi.node('there', 'i:am:here', {
+    version: '1.1.1',
+    optional: true,
+    dev: true,
+    bundled: true,
+    resolved: 'here/it/is',
+    integrity: 'sha1-deadbeef'
+  })
+  t.similar(dep, {
+    name: 'there',
+    address: 'i:am:here',
+    version: '1.1.1',
+    optional: true,
+    dev: true,
+    bundled: true,
+    resolved: 'here/it/is',
+    integrity: 'sha1-deadbeef'
+  }, 'accepts address and options')
+  t.done()
+})
+
+test('hasCycle', t => {
+  const tree = logi.node('root')
+  const dep1 = logi.node('dep1')
+  const dep2 = logi.node('dep2')
+  const dep3 = logi.node('dep3')
+  tree.addDep(dep1.addDep(dep2))
+  t.equal(tree.hasCycle(), false, 'no cycle found for tree')
+  t.equal(dep1.hasCycle(), false, 'no cycle found for dep1')
+  t.equal(dep2.hasCycle(), false, 'no cycle found for dep2')
+  dep2.addDep(dep1)
+  t.equal(tree.hasCycle(), false, 'no cycle found for tree')
+  t.equal(dep1.hasCycle(), true, 'dep1 has a cycle')
+  t.equal(dep2.hasCycle(), true, 'dep2 has a cycle')
+  dep2.delDep(dep1).addDep(dep3)
+  dep3.addDep(dep1)
+  t.equal(tree.hasCycle(), false, 'no cycle found for tree')
+  t.equal(dep1.hasCycle(), true, 'dep1 has transitive cycle')
+  t.equal(dep2.hasCycle(), true, 'dep2 has transitive cycle')
+  t.equal(dep3.hasCycle(), true, 'dep3 has transitive cycle')
+  dep3.addDep(tree)
+  dep1.addDep(tree)
+  tree.addDep(dep2)
+  t.equal(tree.hasCycle(), true, 'complex cycle resolved successfully')
+  const selfRef = logi.node('selfRef')
+  selfRef.addDep(selfRef)
+  t.equal(selfRef.hasCycle(), true, 'self-referencing dep handled')
+  t.done()
+})
+
+test('forEachAsync', t => {
+  const tree = logi.node('root')
+  const dep1 = logi.node('dep1')
+  const dep2 = logi.node('dep2')
+  const dep3 = logi.node('dep3')
+
+  tree.addDep(dep1.addDep(dep2.addDep(dep3)))
+  let found = []
+  return tree.forEachAsync((dep, next) => {
+    return Promise.resolve(found.push(dep))
+  })
+  .then(() => {
+    t.deepEqual(found, [tree], 'no children unless next is used')
+    found = []
+    return tree.forEachAsync((dep, next) => {
+      return next().then(() => found.push(dep))
+    })
+  })
+  .then(() => {
+    t.deepEqual(found, [dep3, dep2, dep1, tree], 'next() iterates down')
+    found = []
+    return tree.forEachAsync((dep, next) => {
+      found.push(dep)
+      return next()
+    })
+  })
+  .then(() => {
+    t.deepEqual(found, [tree, dep1, dep2, dep3], 'next() can be called after')
+    found = []
+    const mapFn = (dep, next) => {
+      found.push(dep)
+      return next()
+    }
+    let usedFakePromise = false
+    const fakeP = {
+      map (arr, fn) {
+        usedFakePromise = true
+        return BB.map(arr, fn)
+      },
+      resolve: BB.resolve
+    }
+    return tree.forEachAsync(mapFn, {Promise: fakeP})
+    .then(() => {
+      t.deepEqual(found, [tree, dep1, dep2, dep3], 'next() can be called after')
+      t.ok(usedFakePromise, 'used fake promise')
+    })
+  })
+  .then(() => {
+    found = []
+    dep3.addDep(tree)
+    return tree.forEachAsync((dep, next) => {
+      found.push(dep)
+      return next()
+    })
+  })
+  .then(() => {
+    t.deepEqual(found, [tree, dep1, dep2, dep3], 'handled cycle correctly')
+  })
+})
+
+test('forEach', t => {
+  const tree = logi.node('root')
+  const dep1 = logi.node('dep1')
+  const dep2 = logi.node('dep2')
+  const dep3 = logi.node('dep3')
+
+  tree.addDep(dep1.addDep(dep2.addDep(dep3)))
+  let found = []
+  tree.forEach((dep, next) => {
+    return found.push(dep)
+  })
+  t.deepEqual(found, [tree], 'no children unless next is used')
+
+  found = []
+  tree.forEach((dep, next) => {
+    next()
+    found.push(dep)
+  })
+  t.deepEqual(found, [dep3, dep2, dep1, tree], 'next() iterates down')
+
+  found = []
+  tree.forEach((dep, next) => {
+    found.push(dep)
+    next()
+  })
+  t.deepEqual(found, [tree, dep1, dep2, dep3], 'next() can be called after')
+
+  found = []
+  dep3.addDep(tree)
+  tree.forEach((dep, next) => {
+    found.push(dep)
+    next()
+  })
+  t.deepEqual(found, [tree, dep1, dep2, dep3], 'handles cycles correctly')
+
+  t.done()
+})
+
+test('atAddr', t => {
   const pkgLock = {
     dependencies: {
       'a': {
